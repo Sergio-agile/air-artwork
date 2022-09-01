c3721ad3d242efc5e25b9814996e87a7af623f1c
Luke Bullen <lukeabullen@gmail.com>
2022-09-01 16:50:03 +0100

    changes in about, show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,4 +11,10 @@ class PagesController < ApplicationController
 
   def artwork
   end
+
+  def contact_us
+  end
+
+  def about_us
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "listings", to: "pages#listings"
+  get "contact_us", to: "pages#contact_us"
+  get "about_us", to: "pages#about_us"
   # get "artwork", to: "pages#artwork"
   # Defines the root path route ("/")
   # root "articles#index"
