d12854b2a53f11014665f81bcee8449cb4b96e04
James Carlon james.carlon1984@outlook.com <james.carlon1984@outlook.com>
2022-09-02 15:01:41 +0200

    search bar done

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -2,4 +2,11 @@ class Artwork < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
   has_many :bookings, dependent: :destroy
+
+  include PgSearch::Model
+
+  pg_search_scope :search_by_content, against: [ :title, :description ],
+  using: {
+    tsearch: { prefix: true }
+    }
 end
